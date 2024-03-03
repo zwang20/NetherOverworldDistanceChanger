4c23a284932e37611203ea962c2cef61988434ec
SushiLeader <lukas.leung@hotmail.com>
2024-03-03 20:57:34 +0800

    add client support

---
+++ src/client/java/today.qwq.netheroverworlddistancechanger.netheroverworlddistancechanger/NetherOverworldDistanceChangerClient.java
@@ -0,0 +1,10 @@
+package today.qwq.netheroverworlddistancechanger.netheroverworlddistancechanger;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NetherOverworldDistanceChangerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
