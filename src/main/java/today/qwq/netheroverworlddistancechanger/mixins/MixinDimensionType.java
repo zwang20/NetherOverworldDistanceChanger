165752806a3181604d8b7b9a1755ad30e6b2d394
SushiLeader <lukas.leung@hotmail.com>
2024-03-04 18:26:10 +0800

    using a better way to check nether

--- src/main/java/today/qwq/netheroverworlddistancechanger/mixins/MixinDimensionType.java
+++ src/main/java/today/qwq/netheroverworlddistancechanger/mixins/MixinDimensionType.java
@@ -1,16 +1,24 @@
 package today.qwq.netheroverworlddistancechanger.mixins;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.util.Identifier;
 import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import today.qwq.netheroverworlddistancechanger.NetherOverworldDistanceChanger;
 
+import java.util.Objects;
+
 @Mixin(DimensionType.class)
 public class MixinDimensionType {
+    @Shadow @Final private Identifier effects;
+
     @ModifyReturnValue(method = "coordinateScale", at = @At("TAIL"))
-    private static double modifyWorldScale(double scale) {
-        if (scale == 8) {
+    private double modifyWorldScale(double scale) {
+        if (Objects.equals(this.effects.toString(), "minecraft:the_nether")) {
+            NetherOverworldDistanceChanger.logger.info("1");
             return NetherOverworldDistanceChanger.CONFIG.distance();
         }
         return scale;
